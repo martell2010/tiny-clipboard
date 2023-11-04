dd9e77a873e1f7e4a6efa12ee6997c8dea10045a
martell <sergiy.listvagov@sdkfinance.com>
2023-11-04 12:43:56 +0200

    Add svg loader and base icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from "path";
-import webExtension from "vite-plugin-web-extension";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import webExtension from 'vite-plugin-web-extension';
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
     extensions: ['.mjs', '.js', '.ts', '.json', '.vue'],
   },
   plugins: [
     vue(),
+    svgLoader({
+      defaultImport: 'component',
+    }),
     webExtension({
-      manifest: "manifest.json"
-    })
+      manifest: 'manifest.json',
+    }),
   ],
-})
+});
